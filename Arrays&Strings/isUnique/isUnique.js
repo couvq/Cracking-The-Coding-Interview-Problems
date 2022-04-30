e475a93010e660f5d509ad17a538e97e8cb81374
couvq <quentincouvelaire@gmail.com>
2022-04-30 18:07:51 -0400

    implemented brute force solution for isUnique problem

--- Arrays&Strings/isUnique/isUnique.js
+++ Arrays&Strings/isUnique/isUnique.js
@@ -2,8 +2,18 @@
     What if you cannot use additional data structures?
 */
 
-const isUnique = (string) => {
+const isUniqueBruteForce = (string) => {
+    for(let i = 0; i < string.length; i++) {
+        for(let j = 0; j < string.length; j++) {
+            if(i === j) j++; // skip current character in evaluation
+            if(string[i] === string[j]) return false; // a duplicate exhists
+        }
+    }
+    return true;
+}
 
+const isUnique = (string) => {
+    return isUniqueBruteForce(string);
 }
 
 module.exports = isUnique;
--- Arrays&Strings/isUnique/isUnique.test.js
+++ Arrays&Strings/isUnique/isUnique.test.js
@@ -1,1 +1,5 @@
 const isUnique = require('./isUnique');
+
+test('empty array is unique', () => {
+    expect(isUnique([])).toBe(true);
+  });
